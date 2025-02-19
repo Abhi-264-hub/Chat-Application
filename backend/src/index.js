1e8d76905b4cfe240ef91f1e6c1096903877685c
Abhishek Kumar <abhishekkumar.06099@gmail.com>
2025-02-19 23:06:15 +0530

    Added frontend routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,16 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js"
 import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/db.js";
+import cors from "cors"
 
 dotenv.config()
 const app=express();
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:5173/",
+    credentials:true
+}))
 const PORT=process.env.PORT
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoute)
