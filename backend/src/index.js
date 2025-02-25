7fda16bc04ff57d351b6cda9d1e5feb7c0d982c5
Abhishek Kumar <abhishekkumar.06099@gmail.com>
2025-02-25 22:56:21 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,11 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js"
 import cookieParser from "cookie-parser"
+import path from "path"
+
+
+
+
 import { connectDB } from "./lib/db.js";
 import cors from "cors"
 import { app,server } from "./lib/socket.js";
@@ -15,9 +20,22 @@ app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
 }))
+
+
 const PORT=process.env.PORT
+const __dirname=path.resolve();
+
+
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoute)
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get("*",(req,res)=>{
+       res.sendFile(path.join(__dirname,"../frontend","dist","index.html")) 
+
+    })
+}
 
 server.listen(PORT,()=>{
     console.log("Server is Running on port PORT:"+PORT);
